13bb13663cbc65f9cf6a7e6dd1fbd239505b1017
Roberto Ronderos <robertoronderosjr@gmail.com>
2023-07-14 18:27:49 -0400

    latests

--- app/src/components/IdeasForm.js
+++ app/src/components/IdeasForm.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import '../styles/IdeasForm.css';
-import axios from 'axios';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { startIdeation, receiveIdeas, ideationError } from '../actions/ideationActions';
+import axios from 'axios';
 
-const IdeationForm = () => {
+const IdeasForm = () => {
   const dispatch = useDispatch();
+  const [technique, setTechnique] = useState('');
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -14,17 +14,28 @@ const IdeationForm = () => {
     const technique = event.target.elements.technique.value;
     const temperature = event.target.elements.temperature.value;
     const model = event.target.elements.model.value;
-    const numIdeas = event.target.elements.numIdeas.value;
-    const tokensPerIdea = event.target.elements.tokensPerIdea.value;
-    const maximumTokens = event.target.elements.maximumTokens.value;
+    const numIdeas = event.target.elements.numIdeas?.value;
+    const tokensPerIdea = event.target.elements.tokensPerIdea?.value;
+    const maximumTokens = event.target.elements.maximumTokens?.value;
+
+    const postData = {
+      problemContext,
+      problemStatement,
+      technique,
+      temperature,
+      model,
+    };
+
+    if (['brainstorming', 'synectics'].includes(technique)) {
+      postData.numIdeas = numIdeas;
+      postData.tokensPerIdea = tokensPerIdea;
+      postData.maximumTokens = maximumTokens;
+    }
+
     dispatch(startIdeation(problemContext, problemStatement, technique, temperature, model, numIdeas, tokensPerIdea, maximumTokens));
     try {
       const baseURL = 'http://127.0.0.1:5001/conceptcloud-38d0c/us-central1'
-      const response = await axios.post(`${baseURL}/ideation`, {
-        problemContext,
-        problemStatement,
-        technique,
-      });
+      const response = await axios.post(`${baseURL}/ideation`, postData);
 
       // Remove the numbers from the ideas
       const ideas = response.data.ideas.map((idea) => idea.replace(/^\d+\.\s*/, ''));
@@ -47,40 +58,48 @@ const IdeationForm = () => {
       </label>
       <label>
         Technique:
-        <select name="technique" required>
-          <option value="">--Please choose an option--</option>
+        <select name="technique" required onChange={(e) => setTechnique(e.target.value)}>
+          <option value="">Select a technique...</option>
           <option value="brainstorming">Brainstorming</option>
           <option value="synectics">Synectics</option>
           <option value="morphologicalAnalysis">Morphological Analysis</option>
           <option value="scamper">SCAMPER</option>
-          <option value="provocation">Provocation-Based Ideation</option>
+          <option value="provocation">Provocation</option>
           <option value="sit">Systematic Inventive Thinking (SIT)</option>
           <option value="sixThinkingHats">Six Thinking Hats</option>
         </select>
       </label>
       <label>
         Temperature:
-        <input type="number" name="temperature" min="0" max="1" step="0.1" required />
+        <input type="number" name="temperature" min="0" max="1" step="0.1" defaultValue="0.5" required />
       </label>
       <label>
         Model:
-        <input type="text" name="model" required />
+        <select name="model" required>
+          <option value="">Select a model...</option>
+          <option value="gpt-3.5-turbo">gpt-3.5-turbo</option>
+          <option value="gpt-4">gpt-4</option>
+        </select>
       </label>
-      <label>
-        Number of Ideas:
-        <input type="number" name="numIdeas" min="1" required />
-      </label>
-      <label>
-        Tokens Per Idea:
-        <input type="number" name="tokensPerIdea" min="1" required />
-      </label>
-      <label>
-        Maximum Tokens:
-        <input type="number" name="maximumTokens" min="1" required />
-      </label>
+      {['brainstorming', 'synectics'].includes(technique) && (
+        <>
+          <label>
+            Number of Ideas:
+            <input type="number" name="numIdeas" min="1" defaultValue="5" required />
+          </label>
+          <label>
+            Tokens Per Idea:
+            <input type="number" name="tokensPerIdea" min="1" defaultValue="20" required />
+          </label>
+          <label>
+            Maximum Tokens:
+            <input type="number" name="maximumTokens" min="1" defaultValue="500" required />
+          </label>
+        </>
+      )}
       <button type="submit">Generate Ideas</button>
     </form>
   );
 };
 
-export default IdeationForm;
+export default IdeasForm;
