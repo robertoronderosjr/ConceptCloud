d5654d476d957032a0ec1e7f1dbd660eabf95cbd
Roberto Ronderos <robertoronderosjr@gmail.com>
2023-07-14 18:17:55 -0400

    styoles

--- app/src/components/IdeasForm.js
+++ app/src/components/IdeasForm.js
@@ -12,7 +12,12 @@ const IdeationForm = () => {
     const problemContext = event.target.elements.problemContext.value;
     const problemStatement = event.target.elements.problemStatement.value;
     const technique = event.target.elements.technique.value;
-    dispatch(startIdeation(problemContext, problemStatement, technique));
+    const temperature = event.target.elements.temperature.value;
+    const model = event.target.elements.model.value;
+    const numIdeas = event.target.elements.numIdeas.value;
+    const tokensPerIdea = event.target.elements.tokensPerIdea.value;
+    const maximumTokens = event.target.elements.maximumTokens.value;
+    dispatch(startIdeation(problemContext, problemStatement, technique, temperature, model, numIdeas, tokensPerIdea, maximumTokens));
     try {
       const baseURL = 'http://127.0.0.1:5001/conceptcloud-38d0c/us-central1'
       const response = await axios.post(`${baseURL}/ideation`, {
@@ -20,7 +25,11 @@ const IdeationForm = () => {
         problemStatement,
         technique,
       });
-      dispatch(receiveIdeas(response.data.ideas));
+
+      // Remove the numbers from the ideas
+      const ideas = response.data.ideas.map((idea) => idea.replace(/^\d+\.\s*/, ''));
+
+      dispatch(receiveIdeas(ideas));
     } catch (error) {
       dispatch(ideationError(error.toString()));
     }
@@ -48,6 +57,26 @@ const IdeationForm = () => {
           <option value="sit">Systematic Inventive Thinking (SIT)</option>
           <option value="sixThinkingHats">Six Thinking Hats</option>
         </select>
+      </label>
+      <label>
+        Temperature:
+        <input type="number" name="temperature" min="0" max="1" step="0.1" required />
+      </label>
+      <label>
+        Model:
+        <input type="text" name="model" required />
+      </label>
+      <label>
+        Number of Ideas:
+        <input type="number" name="numIdeas" min="1" required />
+      </label>
+      <label>
+        Tokens Per Idea:
+        <input type="number" name="tokensPerIdea" min="1" required />
+      </label>
+      <label>
+        Maximum Tokens:
+        <input type="number" name="maximumTokens" min="1" required />
       </label>
       <button type="submit">Generate Ideas</button>
     </form>
